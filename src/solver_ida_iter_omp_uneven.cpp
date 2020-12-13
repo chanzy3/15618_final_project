c164d2d85b7de4d52600da22b99d630c3284ff02
Art Zhu <tianez@andrew.cmu.edu>
2020-12-13 00:01:14 -0500

    completed subroutine, pending steal

--- src/solver_ida_iter_omp_uneven.cpp
+++ src/solver_ida_iter_omp_uneven.cpp
@@ -123,6 +123,7 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
     // to mark for other threads to see
     node_iter_t *local_path = worker_states[thread_id]->local_path;
     int *starting_depth = &worker_states[thread_id]->starting_depth;
+    int *local_path_d = &worker_states[thread_id]->local_path_d;
 
     // where to work on sub problem
     node_iter_t *private_local_path = (node_iter_t *) malloc(siz);
@@ -137,6 +138,7 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
      */
     while (1) {
       *starting_depth = -1;
+      *local_path_d = -1;
 
       omp_set_lock(&task_creation_lock);
 
@@ -250,6 +252,7 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
 
 
               *starting_depth = path_d + 1;
+              *local_path_d = path_d + 1;
 
               break;
             }
@@ -261,7 +264,11 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
 
       if (*starting_depth != -1) {
         // double s = CycleTimer::currentSeconds();
-        int local_solution_length = search_iter_omp_uneven_helper(corner_db, local_path, starting_depth, private_local_path, private_starting_depth, bound);
+        int local_solution_length = search_iter_omp_uneven_helper(corner_db,
+                                                                  path, solution_length,
+                                                                  local_path, starting_depth, local_path_d,
+                                                                  &(worker_states[thread_id]->lock),
+                                                                  private_local_path, private_starting_depth, bound);
         // double e = CycleTimer::currentSeconds();
         // OMP_PRINTF("r: %f ms\n", (e - s) * 1000.0f);
 
@@ -276,12 +283,12 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
 #pragma omp atomic // write
           found |= 1; // TODO(tianez): hack true;
 
-          omp_set_lock(&task_creation_lock);
+          omp_set_lock(&solution_lock);
 
           memcpy(path, local_path, MAX_DEPTH * sizeof(node_iter_t));
           *solution_length = local_solution_length;
 
-          omp_unset_lock(&task_creation_lock);
+          omp_unset_lock(&solution_lock);
 
           goto serial_task_creation_end;
         }
@@ -308,14 +315,161 @@ void SolverIdaIterOmpUneven::search_iter_omp_uneven(paracube::CornerPatternDatab
   omp_destroy_lock(&task_creation_lock);
 }
 
-int SolverIdaIterOmpUneven::search_iter_omp_uneven_helper(paracube::CornerPatternDatabase *corner_db, node_iter_t *local_path, int *starting_depth, node_iter_t *private_local_path, int *private_starting_depth, int bound) {
-  return search_iter_omp_uneven_helper_private(corner_db, local_path, *starting_depth, bound);
+#define PRIVATE_SUB_PROBLEM_DEPTH 3
+
+int SolverIdaIterOmpUneven::search_iter_omp_uneven_helper(paracube::CornerPatternDatabase *corner_db,
+                                                          node_iter_t path[MAX_DEPTH], int *solution_length,
+                                                          node_iter_t *local_path, int *starting_depth, int *local_path_d,
+                                                          omp_lock_t *local_lock,
+                                                          node_iter_t *private_local_path, int *private_starting_depth, int bound) {
+  node_iter_t *root = &(local_path[*starting_depth - 1]);
+
+  if (test_converge(&(root->cube))) {
+    return *local_path_d;
+  }
+
+  while (1) {
+    *private_starting_depth = -1;
+
+    omp_set_lock(local_lock);
+    while (1) {
+      node_iter_t *n_curr = &(local_path[*local_path_d - 1]); // curr node
+      node_iter_t *n_prev = *local_path_d == 1 ? NULL : n_curr - 1;
+
+#ifdef PRUNE
+      if (n_prev != NULL) {
+        while (can_prune(n_prev->op - 1, n_curr->op)) {
+          n_curr->op++; // skip an op
+        }
+      }
+#endif
+
+      // PPATH(path, path_d);
+      // PWPATH(path, path_d);
+
+      if (n_curr->op >= TRANSITION_COUNT) {
+        // TODO(tianez): good place?
+        if (found) {
+          omp_unset_lock(local_lock);
+          return -1;
+        }
+        // finished all ops
+
+        if (*local_path_d == *starting_depth) {
+          // TODO(tianez):
+          omp_unset_lock(local_lock);
+          return -1; // don't free root
+        } else {
+          n_prev->min = std::min(n_prev->min, n_curr->min);
+
+          // node_iter_destroy(n_curr); // stack.pop
+          // path[path_d - 1] = NULL;
+          (*local_path_d)--;
+
+          continue; // resume work on previous problem, current problem complete
+        }
+      } else {
+        node_iter_t *n_next = n_curr + 1;
+        node_iter_cpy(n_next, n_curr);
+
+        // generate next problem:
+        // input: cube, g, d
+        // problem internal state: min, op (iterate from 0 - 17)
+
+
+#ifdef COUNT_TRANSITIONS
+#pragma omp atomic
+        ida_iter_omp_num_transitions++;
+#endif
+
+        // cube
+        transition[n_curr->op](&(n_next->cube));
+        (n_curr->op)++; // go to next op
+        // g
+        n_next->g += cost(n_next, n_curr);
+        // d
+        n_next->d++;
+        // min
+        n_next->min = INFTY; // TODO(tianez): need this? is this optimization?
+        // op
+        n_next->op = 0; // start from first op
+
+        // Keep next problem or not ? depending on f and convergence, choices:
+        // 1. convergence:  return found
+        // 2. f > bound:    discard, update current problem min
+        // 3. f <= bound:   keep, next iteration will work on new problem
+        int f = n_next->g + h(corner_db, n_next);
+
+        if (test_converge(&(n_next->cube))) {
+          // node_iter_destroy(n_next);
+
+          n_curr->min = FOUND;
+          omp_unset_lock(local_lock);
+          return *local_path_d; // return found (solution_length)
+        }
+
+        if (f > bound) { // discard
+          n_curr->min = std::min((int) n_curr->min, f);
+          // node_iter_destroy(n_next);
+        } else {
+          // path[path_d] = n_next; // stack.push
+          // OMP_PRINTF("t_%.2d: bound(%d) - *local_path_d(%d) > PSPD(%d)\n", omp_get_thread_num(), bound, *local_path_d, PRIVATE_SUB_PROBLEM_DEPTH);
+
+          if (bound - *local_path_d > PRIVATE_SUB_PROBLEM_DEPTH) {
+            (*local_path_d)++;
+          } else {
+            // go to sub problem
+            memcpy(private_local_path, local_path, MAX_DEPTH * sizeof(node_iter_t));
+            *private_starting_depth = (*local_path_d) + 1;
+            break;
+          }
+        }
+      }
+    }
+    omp_unset_lock(local_lock);
+
+    // work on sub problem
+    if (*private_starting_depth != -1) {
+      // double s = CycleTimer::currentSeconds();
+      int private_local_solution_length = search_iter_omp_uneven_helper_private(corner_db, private_local_path, bound, *private_starting_depth);
+      // double e = CycleTimer::currentSeconds();
+      // OMP_PRINTF("r: %f ms\n", (e - s) * 1000.0f);
+
+      // TODO(tianez): update global and sync
+      if (curr_iter_mean > (int) private_local_path[*private_starting_depth - 1].min) {
+#pragma omp atomic // write
+        curr_iter_mean -= curr_iter_mean - ((int) private_local_path[*private_starting_depth - 1].min);
+      }
+
+
+      if (private_local_solution_length > 0) {
+#pragma omp atomic // write
+        found |= 1; // TODO(tianez): hack true;
+
+        omp_set_lock(&solution_lock);
+
+        memcpy(path, private_local_path, MAX_DEPTH * sizeof(node_iter_t));
+        *solution_length = private_local_solution_length;
+
+        omp_unset_lock(&solution_lock);
+
+        return -1; // make sure upper level don't overwrite result here.
+      }
+
+      // TODO(tianez): solution length
+    }
+  }
+
+  // return search_iter_omp_uneven_helper_private(corner_db, local_path, *starting_depth, bound);
   // return search_iter_omp_uneven_helper_private(corner_db, private_local_path, *private_starting_depth, bound);
+  return -1;
 }
 
 int SolverIdaIterOmpUneven::search_iter_omp_uneven_helper_private(paracube::CornerPatternDatabase *corner_db, node_iter_t path[MAX_DEPTH], int bound, int starting_depth) {
   node_iter_t *root = &(path[starting_depth - 1]);
   int path_d = starting_depth;
+
+  // OMP_PRINTF("t_%.2d: private starting depth: %d\n", omp_get_thread_num(), starting_depth);
 
   if (test_converge(&(root->cube))) {
     return path_d;
--- src/solver_ida_iter_omp_uneven.h
+++ src/solver_ida_iter_omp_uneven.h
@@ -14,9 +14,13 @@ public:
   SolverIdaIterOmpUneven(int num_threads) : SolverIdaIterOmpBase(num_threads) {
     this->num_threads = num_threads;
     worker_states = (WorkerState **) malloc(num_threads * sizeof(WorkerState *));
+
+    omp_init_lock(&solution_lock);
   }
 
   ~SolverIdaIterOmpUneven() {
+    omp_destroy_lock(&solution_lock);
+
     for (int i=0; i<num_threads; i++) {
       omp_destroy_lock(&(worker_states[i]->lock));
       free(worker_states[i]);
@@ -31,7 +35,11 @@ private:
   bool ida_solve_iter_omp_uneven(cube_t *cube, int solution[MAX_DEPTH], int *num_steps);
 
   void search_iter_omp_uneven(paracube::CornerPatternDatabase *corner_db, node_iter_t path[MAX_DEPTH], int *solution_length, int bound);
-  int search_iter_omp_uneven_helper(paracube::CornerPatternDatabase *corner_db, node_iter_t *local_path, int *starting_depth, node_iter_t *private_local_path, int *private_starting_depth, int bound);
+  int search_iter_omp_uneven_helper(paracube::CornerPatternDatabase *corner_db,
+                                    node_iter_t path[MAX_DEPTH], int *solution_length,
+                                    node_iter_t *local_path, int *starting_depth, int *local_path_d,
+                                    omp_lock_t *local_lock,
+                                    node_iter_t *private_local_path, int *private_starting_depth, int bound);
   int search_iter_omp_uneven_helper_private(paracube::CornerPatternDatabase *corner_db, node_iter_t path[MAX_DEPTH], int bound, int starting_depth);
 
   enum WorkingState {
@@ -43,10 +51,13 @@ private:
     enum WorkingState state;
     int starting_depth;
     node_iter_t local_path[MAX_DEPTH];
+    int local_path_d;
     omp_lock_t lock;
   };
   int num_threads;
   WorkerState **worker_states;
+
+  omp_lock_t solution_lock;
 };
 
 #endif //INC_15618_FINAL_PROJECT_SOLVER_IDA_ITER_OMP_UNEVEN_H
